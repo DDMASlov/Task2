9ef0d1606a531aa9c06567d08360346913ac17c8
DDMASlov <91286146+DDMASlov@users.noreply.github.com>
2021-10-15 18:34:51 +0400

    Add files via upload

---
+++ fl.cpp
@@ -0,0 +1,104 @@
+#include "class.hpp"
+int main()
+{
+    if(autotest() != 0)
+    {
+	return 1;
+    }
+    int size1, size2;
+    double *ptr;
+    cout << "Enter size of vector: ";
+    cin >> size1;
+    if(size1 <= 0)
+    {
+	cout << "Wrong type of size!!!";
+	return 1;
+    }
+    ptr = (double*)malloc(size1*sizeof(double));
+    cout << "FIRST VECTOR" << endl;
+    for(int i = 0; i < size1;i++)
+    {
+	cout << "Enter " << i+1 << " element of vector: ";
+	cin >> ptr[i];
+    }
+    CVector a(ptr, size1);
+    cout << "SECOND VECTOR" << endl;
+    for(int i = 0; i < size1; i++)
+    {
+	cout << "Enter " << i+1 << " element of vector: ";
+	cin >> ptr[i];
+    }
+    CVector b(ptr,size1);
+    int cntrl = 1;
+    cout << "0 - quit" << endl;
+    cout << "1 - add elements to 1" << endl;
+    cout << "2 - add elements to 2" << endl;
+    cout << "3 - sum of vectors" << endl;
+    cout << "4 - dif of vectors" << endl;
+    cout << "5 - scalar product" << endl;
+    while(cntrl != 0)
+    {
+	cin >> cntrl;
+	if((cntrl != 0) && (cntrl != 1) && (cntrl != 2) && (cntrl != 3) && (cntrl != 4) && (cntrl != 5))
+	{
+	    cout << "Wrong command!!" << endl;
+	}
+	else
+	{
+	    if(cntrl == 1)
+	    {
+		cout << "Enter number of adding elements:";
+		cin >> size2;
+		a.AddVec(size2);
+	    }
+	    if(cntrl == 2)
+	    {
+		cout << "Enter number of adding elements:";
+		cin >> size2;
+		b.AddVec(size2);
+	    }
+	    if(cntrl == 3)
+	    {
+		if(a.GetSize() == b.GetSize())
+		{
+		    CVector c = a+b;
+		    cout << "Sum of vectors : ";
+		    c.GetVec();
+		}
+		else
+		{
+		    cout << "Different size of vectors!!" << endl;
+		}
+	    }
+	    if(cntrl == 4)
+	    {
+		if(a.GetSize() == b.GetSize())
+		{
+		    CVector d = a - b;
+		    cout << "Difference of vecotrs: ";
+		    d.GetVec();
+		}
+		else
+		{
+		    cout << "Different size of vectors!!" << endl;
+		}
+	    }
+	    if(cntrl == 5)
+	    {
+		if(a.GetSize() == b.GetSize())
+		{
+		    int sc = a*b;
+		    cout << "Scalar product: " << sc << endl;
+		}
+		else
+		{
+		    cout << "Different size of vectors!!" << endl;
+		}
+	    }
+	    if(cntrl == 0)
+	    {
+		return 0;
+	    }
+	}
+    }
+}
